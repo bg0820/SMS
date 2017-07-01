a87ca18f77e8413c20496037c30fc4fca1e0aecb
KimBoGyu <bg0820@naver.com>
2017-07-01 13:49:38 +0900

    Process List Class Logic add

---
+++ ServerMonitoringService/ServerMonitoringService/ProcessList.cpp
@@ -0,0 +1,34 @@
+#include "ProcessList.h"
+
+int ProcessList::update()
+{
+	if (EnumProcesses(processList, sizeof(processList), &processListSize) == 0) // EnumProcesses func FAILED return value is zero;
+		return 0;
+
+	return 1; // SUCCESS
+}
+
+int ProcessList::getCount()
+{
+	if (processListSize != NULL)
+		return  processListSize / sizeof(DWORD);
+	else
+		return 0;
+}
+
+int ProcessList::getPID(const int i)
+{
+	if (processList[i] != NULL && processList[i] != 0)
+		return processList[i];
+	else
+		return 0;
+}
+
+Process ProcessList::getProcess(const DWORD processId)
+{
+	Process process;
+	
+	// TODO : Process Obj return
+
+	return process;
+}
--- ServerMonitoringService/ServerMonitoringService/ProcessList.h
+++ ServerMonitoringService/ServerMonitoringService/ProcessList.h
@@ -1,1 +1,35 @@
-#pragma once
+#ifndef _PROCESSLIST_H_
+#define _PROCESSLIST_H_
+
+#include <Windows.h>
+#include <Psapi.h>
+#include "Process.h"
+
+#pragma comment(lib, "psapi.lib")
+
+class ProcessList
+{
+private:
+	DWORD processList[1024];
+	DWORD processListSize = NULL;
+private:
+	ProcessList()
+	{
+
+	}
+
+	~ProcessList()
+	{
+
+	}
+public:
+	int update(); // process list update
+	int getCount(); // update -> return value is processList count
+	Process getProcess(const DWORD processId); // return value is processId index to Process Obj
+	int getPID(const int i); // return value is num i to process PID value
+							 // TODO: function Add
+							 // getProcessUser, getProcessStatus, getProcessNetworkSend, 
+							 // getProcessNetworkReceive, getProcessDescription
+};
+
+#endif
