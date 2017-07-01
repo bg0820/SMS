3ef5311a9f2fe72b930245d76d52673d1856323d
KimBoGyu <bg0820@naver.com>
2017-07-01 09:44:47 +0900

    Class that gets information about computer memory usage

---
+++ ServerMonitoringService/ServerMonitoringService/SystemMemory.cpp
@@ -0,0 +1,52 @@
+#include "SystemMemory.h"
+
+int SystemMemory::memoryCall()
+{
+	memoryStat.dwLength = sizeof(MEMORYSTATUSEX);
+
+	if (GlobalMemoryStatusEx(&memoryStat) == 0)
+		return  0;
+
+	return 1;
+}
+
+int SystemMemory::getLoadPercent(int &val)
+{
+	if (!memoryCall())
+		return  0;
+
+	val = memoryStat.dwMemoryLoad;
+
+	return 1;
+}
+
+int SystemMemory::getTotalByte(DWORD &val)
+{
+	if (!memoryCall())
+		return 0;
+
+	val = (double)memoryStat.ullTotalPhys;
+
+	return 1;
+}
+
+int SystemMemory::getFreeByte(DWORD &val)
+{
+	if (!memoryCall())
+		return 0;
+
+	val = (double)memoryStat.ullAvailPhys;
+
+	return 1;
+}
+
+int SystemMemory::getUsage(double &val)
+{
+	if (!memoryCall())
+		return 0;
+
+	// Total - Free = Use
+	val = (double)(memoryStat.ullTotalPhys - memoryStat.ullAvailPhys);
+
+	return 1;
+}
--- ServerMonitoringService/ServerMonitoringService/SystemMemory.h
+++ ServerMonitoringService/ServerMonitoringService/SystemMemory.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef _SYSTEMEMORY_H_
+#define _SYSTEMEMORY_H_
+
+#include "windows.h"
+
+class SystemMemory
+{
+private:
+	MEMORYSTATUSEX memoryStat;
+private:
+	int memoryCall();
+public:
+	int getLoadPercent(int &val);
+	int getUsage(double &val);
+	int getTotalByte(DWORD &val);
+	int getFreeByte(DWORD &val);
+};
+
+#endif
