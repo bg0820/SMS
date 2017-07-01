4629120980b33c9a5356452ad0334b48a470db12
KimBoGyu <bg0820@naver.com>
2017-07-01 13:26:59 +0900

    SystemCPU logic add check-in

---
+++ ServerMonitoringService/ServerMonitoringService/SystemCPU.cpp
@@ -0,0 +1,54 @@
+#include "SystemCPU.h"
+
+int SystemCpu::getUsage(double &val)
+{
+	FILETIME sysIdle, sysKernel, sysUser;
+	// sysKernel include IdleTime
+	if (GetSystemTimes(&sysIdle, &sysKernel, &sysUser) == 0) // GetSystemTimes func FAILED return value is zero;
+		return 0;
+
+	if (prevSysIdle.dwLowDateTime != 0 && prevSysIdle.dwHighDateTime != 0)
+	{
+		ULONGLONG sysIdleDiff, sysKernelDiff, sysUserDiff;
+		sysIdleDiff = SubtractTimes(sysIdle, prevSysIdle);
+		sysKernelDiff = SubtractTimes(sysKernel, prevSysKernel);
+		sysUserDiff = SubtractTimes(sysUser, prevSysUser);
+
+		ULONGLONG sysTotal = sysKernelDiff + sysUserDiff;
+		ULONGLONG kernelTotal = sysKernelDiff - sysIdleDiff; // kernelTime - IdleTime = kernelTime, because sysKernel include IdleTime
+
+		if (sysTotal > 0) // sometimes kernelTime > idleTime
+			val = (double)(((kernelTotal + sysUserDiff) * 100.0) / sysTotal);
+	}
+
+	prevSysIdle = sysIdle;
+	prevSysKernel = sysKernel;
+	prevSysUser = sysUser;
+
+	return 1;
+}
+
+// 100 - total = IDLE CPU Usage
+int SystemCpu::getIdleUsage(double &val)
+{
+	double curUsage;
+	if (getUsage(curUsage) == 0)
+		return 0;
+
+	val = 100.0 - curUsage;
+
+	return 1;
+}
+
+// TIME DIFF FUNC
+ULONGLONG SystemCpu::SubtractTimes(const FILETIME one, const FILETIME two)
+{
+	LARGE_INTEGER a, b;
+	a.LowPart = one.dwLowDateTime;
+	a.HighPart = one.dwHighDateTime;
+
+	b.LowPart = two.dwLowDateTime;
+	b.HighPart = two.dwHighDateTime;
+
+	return a.QuadPart - b.QuadPart;
+}
--- ServerMonitoringService/ServerMonitoringService/SystemCPU.h
+++ ServerMonitoringService/ServerMonitoringService/SystemCPU.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef _SYSTEMCPU_H_
+#define _SYSTEMCPU_H_
+
+#include <Windows.h>
+
+typedef unsigned __int64 ULONGLONG;
+
+class SystemCpu
+{
+private:
+	FILETIME prevSysIdle, prevSysKernel, prevSysUser;
+	ULONGLONG SubtractTimes(FILETIME one, FILETIME two);
+public:
+	int getUsage(double &val);
+	int getIdleUsage(double &val);
+	//int getSysUsage(double &val);
+};
+
+#endif
