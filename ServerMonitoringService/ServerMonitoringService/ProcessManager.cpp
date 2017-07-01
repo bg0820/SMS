e219147f83be2ebc41bf3b40c97a15be18583ddb
KimBoGyu <bg0820@naver.com>
2017-07-01 13:50:16 +0900

    Process Manager Class Logic add

---
+++ ServerMonitoringService/ServerMonitoringService/ProcessManager.cpp
@@ -0,0 +1,56 @@
+#include "ProcessManager.h"
+
+int ProcessManager::insert(Process process)
+{
+	if (numCount >= maxLen) // Storage Lack
+		return 0;
+
+	ProcessManager::process[numCount] = process;
+	numCount++;
+
+	return 1;
+}
+
+int ProcessManager::first(Process &data)
+{
+	if (numCount == 0)
+		return FALSE;
+
+	curPosition = 0;
+	data = ProcessManager::process[0];
+
+	return TRUE;
+}
+
+int ProcessManager::next(Process &data)
+{
+	if (curPosition >= numCount - 1) // next data dosen't Exist
+		return FALSE;
+
+	curPosition++;
+	data = ProcessManager::process[curPosition];
+
+	return TRUE;
+}
+
+Process ProcessManager::remove(ProcessManager &processManager)
+{
+	int rpos = curPosition;
+	int num = numCount;
+
+	Process process = ProcessManager::process[rpos];
+
+	for (int i = 0; i < num - 1; i++)
+		ProcessManager::process[i] = ProcessManager::process[i + 1]; // << 1
+
+																	 // << 1
+	numCount--;
+	curPosition--;
+
+	return process;
+}
+
+int ProcessManager::getCount()
+{
+	return numCount;
+}
--- ServerMonitoringService/ServerMonitoringService/ProcessManager.h
+++ ServerMonitoringService/ServerMonitoringService/ProcessManager.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef _PROCESSMANAGER_H_
+#define _PROCESSMANAGER_H_
+
+#include "Process.h"
+
+#define TRUE 1
+#define FALSE 0
+
+class ProcessManager
+{
+private:
+	Process * process;
+	int numCount;
+	int curPosition;
+	int maxLen;
+public:
+	ProcessManager(int len)
+	{
+		maxLen = len;
+		numCount = 0;
+		curPosition = -1;
+		process = new Process[len];
+	}
+
+	~ProcessManager() {
+		delete process;
+	}
+
+	int insert(Process process); // Insert
+	int first(Process &data); // First Move
+	int next(Process &data); // Next Move
+	Process remove(ProcessManager &processManager); // Current Position Object Remove
+	int getCount();
+};
+
+#endif
