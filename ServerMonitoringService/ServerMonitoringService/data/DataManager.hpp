7415909d7f7ee6b0288114f0368d33a78c284f98
KimBoGyu <bg0820@naver.com>
2017-07-13 16:23:43 +0900

    Revert "Revert "ProcessList Update add""
    This reverts commit f52de7e4cb504e7d198d1f79a2964056b1a8839e.

--- ServerMonitoringService/ServerMonitoringService/data/DataManager.hpp
+++ ServerMonitoringService/ServerMonitoringService/data/DataManager.hpp
@@ -2,12 +2,37 @@
 #define _DATAMANAGER_H_
 
 #include <Windows.h>
+#include "../process/ProcessList.hpp"
+#include "../process/Process.hpp"
+
+
+typedef struct system
+{
+	double cpuVal;
+	double cpuIdleVal;
+	DWORD memoryUseVal;
+	DWORD memoryFreeVal;
+	Process *process;
+}System;
+
 
 class DataManager
 {
 private:
-	DWORD processList[1024];
+	DWORD *processes = NULL;
+	System system;
+	ProcessList processListObj = ProcessList();
 public:
+	DataManager()
+	{
+
+	}
+
+	~DataManager()
+	{
+
+	}
+
 	void Update();
 };
 
